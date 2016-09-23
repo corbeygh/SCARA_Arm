5deff04c91a96238bd316e1074bdba751ce96423
EricDiputado <ericdiputado@gmail.com>
2016-09-23 18:50:28 +1200

    Added more processing
    Added loading, drawing, saving

--- JavaCode/ToolPath.java
+++ JavaCode/ToolPath.java
@@ -1,6 +1,4 @@
 
-
-
 /**
  * ToolPath stores motor contol signals (pwm)
  * and motor angles
@@ -25,7 +23,6 @@ import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-
 public class ToolPath
 {
     int n_steps; //straight line segmentt will be broken
@@ -46,7 +43,7 @@ public class ToolPath
     public ToolPath()
     {
         // initialise instance variables
-        n_steps = 50;
+        n_steps = 1;
         theta1_vector = new ArrayList<Double>();
         theta2_vector = new ArrayList<Double>();
         pen_vector = new ArrayList<Integer>();
@@ -58,19 +55,20 @@ public class ToolPath
 
     /**********CONVERT (X,Y) PATH into angles******************/
     public void convert_drawing_to_angles(Drawing drawing, Arm arm, String fname){
-
         // for all points of the drawing...
         for (int i = 0;i < drawing.get_drawing_size()-1;i++){
             // take two points
             PointXY p0 = drawing.get_drawing_point(i);
             PointXY p1 = drawing.get_drawing_point(i+1);
+            if (!p0.get_pen())n_steps = 1;
+            else n_steps = (int)Math.ceil(Math.abs(p1.get_x()-p0.get_x())/5);
             // break line between points into segments: n_steps of them
             for ( int j = 0 ; j< n_steps;j++) { // break segment into n_steps str. lines
-                double x = p0.get_x() + j*(p1.get_x()-p0.get_x())/n_steps;
-                double y = p0.get_y() + j*(p1.get_y()-p0.get_y())/n_steps;
+                double x = p0.get_x() + j*(p1.get_x()-p0.get_x());
+                double y = p0.get_y() + j*(p1.get_y()-p0.get_y());
                 arm.inverseKinematic(x, y);
-                theta1_vector.add(arm.get_theta1()*180/Math.PI);
-                theta2_vector.add(arm.get_theta2()*180/Math.PI);
+                theta1_vector.add(arm.get_theta1());
+                theta2_vector.add(arm.get_theta2());
                 if (p0.get_pen()){
                     pen_vector.add(1);
                 } else {
@@ -78,13 +76,10 @@ public class ToolPath
                 }
             }
         }
+        UI.println(theta1_vector.size());
     }
 
     public void save_angles(String fname){
-        for ( int i = 0 ; i < theta1_vector.size(); i++){
-            UI.printf(" t1=%3.1f t2=%3.1f pen=%d\n",
-                    theta1_vector.get(i),theta2_vector.get(i),pen_vector.get(i));
-        }
 
         try {
             //Whatever the file path is.
@@ -94,8 +89,7 @@ public class ToolPath
             Writer w = new BufferedWriter(osw);
             String str_out;
             for (int i = 1; i < theta1_vector.size() ; i++){
-                str_out = String.format("%3.1f,%3.1f,%d\n",
-                        theta1_vector.get(i),theta2_vector.get(i),pen_vector.get(i));
+                str_out = String.format("%3.1f,%3.1f,%d\n", theta1_vector.get(i),theta2_vector.get(i),pen_vector.get(i));
                 w.write(str_out);
             }
             w.close();
@@ -117,8 +111,23 @@ public class ToolPath
     }
 
     // save file with motor control values
-    public void save_pwm_file(){
-        //...
+    public void save_pwm_file(String fname){
+        try {
+            //Whatever the file path is.
+            File statText = new File(fname);
+            FileOutputStream is = new FileOutputStream(statText);
+            OutputStreamWriter osw = new OutputStreamWriter(is);
+            Writer w = new BufferedWriter(osw);
+            String str_out;
+            for (int i = 1; i < theta1_vector.size() ; i++){
+                str_out = String.format("%4d,%4d,%d%n", pwm1_vector.get(i),pwm2_vector.get(i),pen_vector.get(i)*1000+1000);
+                w.write(str_out);
+            }
+            w.close();
+        } catch (IOException e) {
+            UI.println("Problem writing to the file statsTest.txt");
+        }
+
     }
 
 }
